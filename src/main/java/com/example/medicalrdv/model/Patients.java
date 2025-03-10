9d97a9526bb77e90ca0bae6d728f15bb207da0b8
Eudoxie <mwinkeudoxie@yahoo.fr>
2025-03-11 00:17:25 +0100

    créations de classes ok

---
+++ src/main/java/com/example/medicalrdv/model/Patients.java
@@ -0,0 +1,11 @@
+package com.example.medicalrdv.model;
+
+import jakarta.persistence.Entity;
+import lombok.Data;
+
+
+@Data
+@Entity
+public class Patients {
+
+}
--- src/main/java/com/example/medicalrdv/model/Users.java
+++ src/main/java/com/example/medicalrdv/model/Users.java
@@ -1,11 +1,37 @@
 package com.example.medicalrdv.model;
 
+import java.sql.Date;
+
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.Data;
 
 
 @Data
 @Entity
 public class Users {
+      @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nom;
+    private String Prénom;
+    private String edresse;
+    private String email;
+    private String password;
+    private Date DateNaiss;
+    
+    public Users(String nom, String prénom, String edresse, String email, String password, Date dateNaiss) {
+        this.nom = nom;
+        Prénom = prénom;
+        this.edresse = edresse;
+        this.email = email;
+        this.password = password;
+        DateNaiss = dateNaiss;
+    }
+
+    
 
 }
