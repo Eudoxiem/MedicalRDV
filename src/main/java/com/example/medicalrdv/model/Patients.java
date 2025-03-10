b3a30937587272c1ff629a48102e987ab832ae97
nix-ones <beanixon@hotmail.fr>
2025-03-11 00:25:19 +0100

    class Patients OK

--- src/main/java/com/example/medicalrdv/model/Patients.java
+++ src/main/java/com/example/medicalrdv/model/Patients.java
@@ -1,11 +1,39 @@
 package com.example.medicalrdv.model;
 
+import java.sql.Date;
+
 import jakarta.persistence.Entity;
-import lombok.Data;
 
 
-@Data
+
+
 @Entity
-public class Patients {
+public class Patients extends Users {
+
+    public Patients(String nom, String prénom, String edresse, String email, String password, Date dateNaiss) {
+        super(nom, prénom, edresse, email, password, dateNaiss);
+        //TODO Auto-generated constructor stub
+    }
+
+    private String numeroSecuriteSociale;
+    private String dateNaissance;
+
+
+    public String getNumeroSecuriteSociale() {
+        return this.numeroSecuriteSociale;
+    }
+
+    public void setNumeroSecuriteSociale(String numeroSecuriteSociale) {
+        this.numeroSecuriteSociale = numeroSecuriteSociale;
+    }
+
+    public String getDateNaissance() {
+        return this.dateNaissance;
+    }
+
+    public void setDateNaissance(String dateNaissance) {
+        this.dateNaissance = dateNaissance;
+    }
+
 
 }
--- src/main/java/com/example/medicalrdv/model/Users.java
+++ src/main/java/com/example/medicalrdv/model/Users.java
@@ -32,6 +32,62 @@ public class Users {
         DateNaiss = dateNaiss;
     }
 
+
+    public Long getId() {
+        return this.id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNom() {
+        return this.nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getPrénom() {
+        return this.Prénom;
+    }
+
+    public void setPrénom(String Prénom) {
+        this.Prénom = Prénom;
+    }
+
+    public String getEdresse() {
+        return this.edresse;
+    }
+
+    public void setEdresse(String edresse) {
+        this.edresse = edresse;
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return this.password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Date getDateNaiss() {
+        return this.DateNaiss;
+    }
+
+    public void setDateNaiss(Date DateNaiss) {
+        this.DateNaiss = DateNaiss;
+    }
     
 
 }
